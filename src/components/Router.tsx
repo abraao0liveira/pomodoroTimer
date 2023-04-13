b8c94d711f896a0411550a8b798dcd65cea2182b
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-04-13 16:11:23 -0300

    update Header (styles.ts)

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,23 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { History } from '../pages/History'
-<<<<<<< HEAD
 import { DefaultLayout } from '../layouts/DefaultLayout'
-=======
->>>>>>> main
 
 export function Router() {
   return (
     <Routes>
-<<<<<<< HEAD
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
       </Route>
-=======
       <Route path="/" element={<Home />} />
       <Route path="/history" element={<History />} />
->>>>>>> main
     </Routes>
   )
 }
