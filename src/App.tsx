67c2437f98a834d200519b9a926d57517d78a6aa
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-04-18 15:39:00 -0300

    update Home and global.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from '../styles/themes/default'
-import { GlobalStyle } from '../styles/global'
-import { Router } from '../components/Router'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './components/Router'
 import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
